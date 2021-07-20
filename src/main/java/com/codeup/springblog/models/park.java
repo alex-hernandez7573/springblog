4ab0bcb5f0471315024bbdade28e7de84bcf9f6c
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-07-20 16:03:57 -0500

    didnt finish, will fix later

---
+++ src/main/java/com/codeup/springblog/models/park.java
@@ -0,0 +1,17 @@
+package com.codeup.springblog.models;
+
+public class Park {
+    private String name;
+
+    public park(String name){
+        this.name = name;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
