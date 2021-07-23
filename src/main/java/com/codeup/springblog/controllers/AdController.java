1d75722dd6ec12d25a51d16e7e2471cab0fd42f1
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-07-23 11:51:54 -0500

    my th blocks arent connect with the css/navbar anymore

---
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -0,0 +1,10 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdController {
+//    private final AdRepository adDao;
+//
+//    public AdController (Adrep)
+}
