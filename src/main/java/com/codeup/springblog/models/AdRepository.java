636e660e8741e9d33eaa9e7adfc31e1de283bc7f
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-07-27 14:08:23 -0500

    pushing to try to catch up

---
+++ src/main/java/com/codeup/springblog/models/AdRepository.java
@@ -0,0 +1,10 @@
+package com.codeup.springblog.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+//public class AdRepository extends JpaRepository <Ad, Long> {
+//    Ad findById ( long id);
+//    @Query ("from Ad where a.title like %:term%")
+//    Ad findByTitle (String term);
+//}
